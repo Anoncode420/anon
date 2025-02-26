9460589409f3640dca9c738a60b098e2331fdd79
Anoncode420 <denzidcartoons@gmail.com>
2025-02-26 17:39:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7981384061
 if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-7981384061');
 if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002398411637');
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'otpsms_1');
 if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
-if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'u241258339_sunny');
-if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'w~Sij4N?');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'otpsms_1');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', '_PslMI5t{SGZ?');
