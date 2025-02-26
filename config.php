a165dfba1b270d3ac93449c77445c8f20b90a69e
Anoncode420 <denzidcartoons@gmail.com>
2025-02-26 17:18:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', '__-B3');
-if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'Was_B3');
-if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '5579729798');
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'mr Anon');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'Anoncryptoking');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6725223313');
 
 if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
-if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'BravCc_Bot');
-if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7048214022:AAGS75gaumDRfGtOOXb4YphshInKf9KzArw');
-if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-4194518429');
-if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002021756101');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'MissEmilia_Robot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7981384061:AAHktfbUQpj_LKR0viSnZYLTOqu0spS-S78');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-7981384061');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002398411637');
 
 if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
 if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
